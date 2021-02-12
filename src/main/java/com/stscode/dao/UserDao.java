592670a67dc470ebf4b558958c840b46ee7652ab
kjlee <yyviper@qq.com>
2021-02-13 02:40:12 +0800

    第二次提交

--- src/main/java/com/stscode/dao/UserDao.java
+++ src/main/java/com/stscode/dao/UserDao.java
@@ -12,4 +12,6 @@ public interface UserDao extends JpaRepository<User, Integer>, JpaSpecificationE
 
     public abstract List<User> findByNameLike(String name);
 
+    public abstract User findByUid(Integer uid);
+
 }
